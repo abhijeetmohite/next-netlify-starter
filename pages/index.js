6335cf57330e8dfd79cb914618f0c7c491e07cf8
Abhijeet Mohite <amohite@cybage.com>
2020-12-17 15:16:40 +0530

    gtag script added

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,15 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
+      
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-C32TYHJCLL"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+
+          gtag('config', 'G-C32TYHJCLL');
+        </script>
         <meta name="google-site-verification" content="uqIgiwHvtkvyxBjIumRTPD7JLOV8D3-0xT7gOEZ2EO8" />
         <title>Next.js Abhijeet Starter!</title>
         <link rel="icon" href="/favicon.ico" />
